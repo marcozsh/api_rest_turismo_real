6a4491fdcd05309b4bd7342607a863395f565010
marcozsh <marc.penar@outlook.cl>
2022-09-23 19:00:49 -0300

    add-department-function

--- api_rest/views.py
+++ api_rest/views.py
@@ -133,5 +133,17 @@ class DepartmentView(View):
                 "long_description":i[7]
             } 
             department.append(department_json)
-
         return JsonResponse(list(department), safe=False)
+
+    def post(self, request):
+        django_cursor = connection.cursor()
+        cursor = django_cursor.connection.cursor()
+        json_decode = request.body.decode('utf-8')
+        post_data = json.loads(json_decode)
+        department = cursor.callfunc("FN_ADD_DEPARTMENT",int,[post_data['address'],post_data['status'],post_data['qty_rooms'],post_data['price'],post_data['commune_id'], post_data['department_type'], post_data['shot_description'], post_data['long_description']])
+        connection.commit()
+        department_response = {
+            "response":department
+        } 
+        return JsonResponse(department_response, safe=False) 
+
